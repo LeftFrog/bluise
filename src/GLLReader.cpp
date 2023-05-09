85150ed5ccfc30e1ba6e528c3011dbd29d3db23f
LeftFrog <oleonov.leftfrog@gmail.com>
2023-05-09 18:38:00 +0300

    Made simple refactor in operator>> func

--- src/GLLReader.cpp
+++ src/GLLReader.cpp
@@ -33,9 +33,6 @@ namespace {
         while(!is.eof()) {
             string line;
             string name;
-            string working_directory;
-            string executable;
-            string save_path;
             std::getline(is, line);
             while(line.empty()&&(!is.eof())){
                 std::getline(is, line);
@@ -48,15 +45,12 @@ namespace {
                 throw gll_syntax_error("Syntax Error!\nThere isnt \"{\"!");
             }
             name = line.substr(0, line.length() - 2);
-            working_directory = get_var("working_directory", is);
-            executable = get_var("executable", is);
-            save_path = get_var("save_path", is);
             string var;
             is >> var;
             if(var!="}") {
                 throw gll_syntax_error("Syntax Error!\nThere isnt \"}\"!");
             }
-            games.push_back(Game(name, working_directory, executable, save_path));
+            games.push_back(Game(name, get_var("working_directory", is), get_var("executable", is), get_var("save_path", is)));
         }
         return is;
     }
