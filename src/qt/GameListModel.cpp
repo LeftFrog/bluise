10d410e436b52a500c3fa7dd6d9de71befb77550
LeftFrog <oleonov.leftfrog@gmail.com>
2023-08-22 20:16:19 +0300

    GameListModel integrated successfully and fully operational. Ready to rock

--- src/qt/GameListModel.cpp
+++ src/qt/GameListModel.cpp
@@ -1,12 +1,8 @@
 #include "GameListModel.h"
 
-GameListModel::GameListModel() 
-{
-}
-
 QVariant GameListModel::data(const QModelIndex &index, int role) const
 {
-    if(!index.isValid() || !(index.row() > bluise_core::games.size()) || index.row() < 0) {
+    if(!index.isValid() || index.row() > bluise_core::games.size() || index.row() < 0) {
         return QVariant();
     }
     return (role == Qt::DisplayRole || role == Qt::EditRole) ? (bluise_core::games[index.row()].get_name().c_str()) : QVariant();
--- src/qt/GameListModel.h
+++ src/qt/GameListModel.h
@@ -5,7 +5,7 @@
 class GameListModel : public QAbstractListModel {
 Q_OBJECT
 public:
-    GameListModel();
+    GameListModel(QObject* parent = nullptr) : QAbstractListModel(parent) {}
     QVariant data(const QModelIndex& index, int role) const;
     int rowCount(const QModelIndex& parent = QModelIndex()) const;
     Qt::ItemFlags flags(const QModelIndex& index) const;
