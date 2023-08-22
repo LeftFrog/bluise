37f1bb4e1d78f9f4d08f7a7ae7bbb436f3a372ab
LeftFrog <oleonov.leftfrog@gmail.com>
2023-08-22 18:30:56 +0300

    Added make_yes_no_win func

--- src/command_line_ui/GameLine.cpp
+++ src/command_line_ui/GameLine.cpp
@@ -10,7 +10,7 @@ void make_info_win(const string& info_str) {
     mvwprintw(info, getmaxy(info) - 2, (getmaxx(info) - 4) / 2, "Back");
     wattroff(info, A_STANDOUT);
     char ch;
-    while(ch = wgetch(info)) {
+    while((ch = wgetch(info))) {
         switch(ch) 
         {
         case ENTER:
@@ -18,6 +18,47 @@ void make_info_win(const string& info_str) {
             return;
         }
     }
+}
+
+bool make_yes_no_win(const string& info_str) {
+    clear();
+    WINDOW* sure = newwin(getmaxy(stdscr) / 2 , getmaxx(stdscr) / 2, getmaxy(stdscr) * 1 / 4, getmaxx(stdscr) * 1 / 4);
+    refresh();
+    box(sure, 0, 0);
+    mvwprintw(sure, 1, (getmaxx(sure) - info_str.length()) / 2, info_str.c_str());
+    wattron(sure, A_STANDOUT);
+    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
+    wattroff(sure, A_STANDOUT);
+    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
+    bool yes = true;
+    char ch;
+    while((ch = wgetch(sure))) {
+        switch(ch) 
+        {
+        case RIGHT_ARROW:
+        case LEFT_ARROW:
+        case 'a':
+        case 'd':    
+            if(yes) {
+                mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
+                wattron(sure, A_STANDOUT);
+                mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
+                wattroff(sure, A_STANDOUT);
+                yes = false;
+            }
+            else {
+                wattron(sure, A_STANDOUT);
+                mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
+                wattroff(sure, A_STANDOUT);
+                mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
+                yes = true;
+            }
+            break;
+        case ENTER:
+            delwin(sure);
+            return yes;
+        }
+    } 
 }
 
 void GameLine::update_pos(int _y) {
@@ -52,49 +93,13 @@ int GameLine::enter()
         return 0;
     }
     else if(menu.button()==Menu::DELETE) {
-        clear();
-        WINDOW* sure = newwin(getmaxy(stdscr) / 2 , getmaxx(stdscr) / 2, getmaxy(stdscr) * 1 / 4, getmaxx(stdscr) * 1 / 4);
-        refresh();
-        box(sure, 0, 0);
         string ok = "Are you sure that you want delete " + game->get_name() + "? (y/n): ";
-        mvwprintw(sure, 1, (getmaxx(sure) - ok.length()) / 2, ok.c_str());
-        wattron(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-        wattroff(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-        bool yes = true;
-        char c;
-        while((c = wgetch(sure))) {
-            switch(c) 
-            {
-            case RIGHT_ARROW:
-            case LEFT_ARROW:
-            case 'a':
-            case 'd':    
-                if(yes) {
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    wattroff(sure, A_STANDOUT);
-                    yes = false;
-                }
-                else {
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattroff(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    yes = true;
-                }
-                break;
-            case ENTER:
-                delwin(sure);
-                if(yes) {
-                    bluise_core::delete_game(game->get_name());
-                    bluise_core::saveGLL();
-                }
-                return 1;
-            }
+        if(make_yes_no_win(ok)) {
+            bluise_core::delete_game(game->get_name());
+            bluise_core::saveGLL();
+            return 1;
         }
+        return 0;
     }
     else if(menu.button()==Menu::SHOWINFO) {
         clear();
@@ -119,92 +124,18 @@ int GameLine::enter()
         }
     }
     else if(menu.button()==Menu::BACK) {
-        clear();
-        WINDOW* sure = newwin(getmaxy(stdscr) / 2 , getmaxx(stdscr) / 2, getmaxy(stdscr) * 1 / 4, getmaxx(stdscr) * 1 / 4);
-        refresh();
-        box(sure, 0, 0);
         string ok = "Do you want to back up saves for the "+game->get_name()+"?";
-        mvwprintw(sure, 1, (getmaxx(sure) - ok.length()) / 2, ok.c_str());
-        wattron(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-        wattroff(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-        bool yes = true;
-        char ch;
-        while(ch = wgetch(sure)) {
-            switch(ch) 
-            {
-            case RIGHT_ARROW:
-            case LEFT_ARROW:
-            case 'a':
-            case 'd':    
-                if(yes) {
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    wattroff(sure, A_STANDOUT);
-                    yes = false;
-                }
-                else {
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattroff(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    yes = true;
-                }
-                break;
-            case ENTER:
-                delwin(sure);
-                if(yes) {
-                    bluise_core::back(game->get_name());
-                }
-                return 0;
-            }
+        if(make_yes_no_win(ok)) {
+            bluise_core::back(game->get_name());
         }
+        return 0;
     }
     else if(menu.button()==Menu::RECOVER) {
-        clear();
-        WINDOW* sure = newwin(getmaxy(stdscr) / 2 , getmaxx(stdscr) / 2, getmaxy(stdscr) * 1 / 4, getmaxx(stdscr) * 1 / 4);
-        refresh();
-        box(sure, 0, 0);
         string ok = "Do you want to recover saves for the "+game->get_name()+"?";
-        mvwprintw(sure, 1, (getmaxx(sure) - ok.length()) / 2, ok.c_str());
-        wattron(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-        wattroff(sure, A_STANDOUT);
-        mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-        bool yes = true;
-        char ch;
-        while(ch = wgetch(sure)) {
-            switch(ch) 
-            {
-            case RIGHT_ARROW:
-            case LEFT_ARROW:
-            case 'a':
-            case 'd':    
-                if(yes) {
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    wattroff(sure, A_STANDOUT);
-                    yes = false;
-                }
-                else {
-                    wattron(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 1 / 4, "YES");
-                    wattroff(sure, A_STANDOUT);
-                    mvwprintw(sure, getmaxy(sure) - 2, getmaxx(sure) * 3 / 4 - 1, "NO");
-                    yes = true;
-                }
-                break;
-            case ENTER:
-                delwin(sure);
-                if(yes) {
-                    bluise_core::recover(game->get_name());
-                }
-                return 0;
-            }
+        if(make_yes_no_win(ok)) {
+            bluise_core::recover(game->get_name());
         }
+        return 0;
     }
 }
 
