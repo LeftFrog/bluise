8d1bb172304dc6090c8fbfddf56b2aa9ad41208e
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-03-19 19:56:00 -0600

    Refactored GameListWidget constructor to dynamically add GameWidgets to the layout based on the size of the game list.

--- src/GameListWidget.cpp
+++ src/GameListWidget.cpp
@@ -7,8 +7,15 @@ GameListWidget::GameListWidget(QWidget *parent)
         game_widgets.push_back(new GameWidget(&bluise_core::games[i], this));
     }
     QGridLayout *layout = new QGridLayout();
-    for(int i = 0; i < game_widgets.size(); ++i) {
-        layout->addWidget(game_widgets[i], 0, i);
+    if(game_widgets.size()>=3) {
+        for(int i = 0; i < game_widgets.size(); ++i) {
+            layout->addWidget(game_widgets[i], i/3, i%3);
+        }
+    }
+    else {
+        for(int i = 0; i < game_widgets.size(); ++i) {
+            layout->addWidget(game_widgets[i], 0, i);
+        }
     }
     resize(1280, 300);
     setLayout(layout);
