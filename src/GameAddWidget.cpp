4be253920c2143b8c47967056acadbf8e309172f
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-04-06 10:25:57 -0600

    Fixed issue with image in GameAddWidget

--- src/GameAddWidget.cpp
+++ src/GameAddWidget.cpp
@@ -5,8 +5,9 @@
 #include <filesystem>
 
 GameAddWidget::GameAddWidget(QWidget *parent) : GameOptionsWidget(parent) {
-  for(auto &option : options) {
-    connect(option, &GameOptionWidget::gameChanged, this, &GameAddWidget::changed);
+  for (auto &option : options) {
+    connect(option, &GameOptionWidget::gameChanged, this,
+            &GameAddWidget::changed);
   }
   connect(choose, &ChoosePictureWidget::changed, this, &GameAddWidget::changed);
   applyButton->setText("Add");
@@ -14,11 +15,13 @@ GameAddWidget::GameAddWidget(QWidget *parent) : GameOptionsWidget(parent) {
 
 void GameAddWidget::changed() {
   if (!applyButton->isEnabled()) {
-    if (options["name"]->isChanged() && options["exec"]->isChanged() && choose->isChanged()) {
+    if (options["name"]->isChanged() && options["exec"]->isChanged() &&
+        choose->isChanged()) {
       applyButton->setEnabled(true);
     }
   } else {
-    if (!options["name"]->isChanged() || !options["exec"]->isChanged() || !choose->isChanged()) {
+    if (!options["name"]->isChanged() || !options["exec"]->isChanged() ||
+        !choose->isChanged()) {
       applyButton->setDisabled(true);
     }
   }
@@ -26,39 +29,44 @@ void GameAddWidget::changed() {
 
 void GameAddWidget::apply() {
   try {
-    if (QFile::exists(bluise_core::DOCS + "res/covers/" +
-                      QString::fromStdString(
-                          std::filesystem::path(choose->path().toStdString())
-                              .filename()
-                              .string()))) {
+    QString header = "";
+    QString fileName = QString::fromStdString(
+        std::filesystem::path(choose->path().toStdString())
+            .filename()
+            .string()
+            .substr(0, std::filesystem::path(choose->path().toStdString())
+                               .filename()
+                               .string()
+                               .size() -
+                           4));
+    QString extension = QString::fromStdString(
+        std::filesystem::path(choose->path().toStdString())
+            .filename()
+            .string()
+            .substr(std::filesystem::path(choose->path().toStdString())
+                        .filename()
+                        .string()
+                        .size() -
+                    4));
+    QString path = bluise_core::DOCS + "res/covers/";
+    if (QFile::exists(path + fileName + extension)) {
       int i = 1;
-      while (
-          QFile::exists(bluise_core::DOCS + "res/covers/" +
-                        QString::fromStdString(
-                            std::filesystem::path(choose->path().toStdString())
-                                .filename()
-                                .string()))) {
+      while (QFile::exists(path + fileName + QString::number(i) + extension)) {
         ++i;
       }
       std::filesystem::copy(
           choose->path().toStdString(),
-          (bluise_core::DOCS + "res/covers/").toStdString() +
-              std::filesystem::path(choose->path().toStdString())
-                  .filename()
-                  .string() +
-              std::to_string(i));
+          (path + fileName + QString::number(i) + extension).toStdString());
+      header = fileName + QString::number(i) + extension;
     } else {
       std::filesystem::copy(choose->path().toStdString(),
                             (bluise_core::DOCS + "res/covers").toStdString());
+      header = fileName + extension;
     }
-    QString header = QString::fromStdString(
-        std::filesystem::path(choose->path().toStdString())
-            .filename()
-            .string());
-    bluise_core::games.push_back(Game(options["name"]->text(), options["exec"]->text(),
+    bluise_core::games.push_back(Game(options["name"]->text(),
+                                      options["exec"]->text(),
                                       options["workingDirectory"]->text(),
                                       options["savePath"]->text(), header));
-    qDebug() << "Game added!" << bluise_core::games.size();
   } catch (bluise_error &err) {
     QMessageBox::critical(this, "Error", QString::fromStdString(err.what()));
     return;
