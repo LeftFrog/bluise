666e4610412ed4d0b0a07402943cfd1edbe9ad26
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-05-02 20:26:11 -0600

    Fixed issue in SearchBar

--- src/SearchBar.cpp
+++ src/SearchBar.cpp
@@ -1,6 +1,13 @@
 #include "SearchBar.h"
+#include <QApplication>
+#include <QStyleHints>
 
 SearchBar::SearchBar(QWidget *parent) : QLineEdit(parent) {
   setPlaceholderText(" Search game by name");
+  setStyleSheet("QLineEdit { border-radius: 5px; }");
+  connect(QApplication::styleHints(), &QStyleHints::colorSchemeChanged, this, &SearchBar::changeTheme);
+}
+
+void SearchBar::changeTheme(Qt::ColorScheme scheme) {
   setStyleSheet("QLineEdit { border-radius: 5px; }");
 }
--- src/SearchBar.h
+++ src/SearchBar.h
@@ -4,4 +4,6 @@
 class SearchBar : public QLineEdit {
 public:
   SearchBar(QWidget *parent = nullptr);
+private slots:
+  void changeTheme(Qt::ColorScheme scheme);
 };
