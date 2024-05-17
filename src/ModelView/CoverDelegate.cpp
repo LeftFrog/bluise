7742f1ee472b2450467c7c706d46069051fd8373
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-05-16 18:41:30 -0600

    Now CoverDelegate can draw item without cover

--- src/ModelView/CoverDelegate.cpp
+++ src/ModelView/CoverDelegate.cpp
@@ -5,13 +5,16 @@
 void CoverDelegate::paint(QPainter *painter, const QStyleOptionViewItem &option, const QModelIndex &index) const {
   if (index.isValid()) {
     QRect rect = option.rect;
-    QPixmap pixmap = index.data(Qt::UserRole).value<QPixmap>();//.scaled(rect.size(), Qt::IgnoreAspectRatio, Qt::SmoothTransformation)
+    QPixmap pixmap = index.data(Qt::UserRole).value<QPixmap>();
     QPainterPath path;
     path.addRoundedRect(rect, 5, 5);
-    //painter->setRenderHint(QPainter::Antialiasing);
     painter->setClipPath(path);
     painter->setPen(Qt::NoPen);
-    painter->drawPixmap(rect, pixmap);
+    if(pixmap.isNull()) {
+      painter->drawRect(rect);
+    } else {
+      painter->drawPixmap(rect, pixmap);
+    }
     if(option.state & QStyle::State_Selected) {
       painter->setCompositionMode(QPainter::CompositionMode_SourceOver);
       QColor color = option.palette.highlight().color(); 
