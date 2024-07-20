513b1a30311e4f8823093369c8dfaf7e8f5355ff
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-07-20 14:14:44 -0600

    Refactored starter.cpp

--- src/starter.cpp
+++ src/starter.cpp
@@ -1,14 +1,10 @@
 #include <QtWidgets>
 #include "MainWindow.h"
-#include "BluiseCore/bluise.h"
 
-int main(int argc, char **argv) {
-  QApplication a(argc, argv);
-
-  a.setWindowIcon(QIcon("/Users/leftfrog/Projects/bluise/res/1024-mac-1024.png"));
-  
-  MainWindow w;
-  w.show();
-
-  return a.exec();
+int main(int argc, char** argv) {
+    QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("/Users/leftfrog/Projects/bluise/res/1024-mac-1024.png"));
+    MainWindow w;
+    w.show();
+    return a.exec();
 }
