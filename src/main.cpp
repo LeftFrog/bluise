f37592bf1c3097ea84c795df87387ed9fa8a79d3
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-07-21 22:33:40 -0600

    Fixed cmake file

---
+++ src/main.cpp
@@ -0,0 +1,10 @@
+#include <QtWidgets>
+#include "MainWindow.h"
+
+int main(int argc, char** argv) {
+    QApplication a(argc, argv);
+    a.setWindowIcon(QIcon("/Users/leftfrog/Projects/bluise/res/1024-mac-1024.png"));
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
