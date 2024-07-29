3c873ba2983e21044a2206aa8b478c798c1b81a8
Leonov Oleksandr <oleonov.leftfrog@gmail.com>
2024-07-29 15:42:46 -0600

    Connected clicked slot of the buttons in the AddGameWidget class to the slots

--- src/Widgets/AddGameWidget.cpp
+++ src/Widgets/AddGameWidget.cpp
@@ -9,15 +9,11 @@ AddGameWidget::AddGameWidget(QWidget* parent) : QWidget(parent) {
 
     BeautifulUi::OptionButton* addLocalInstalledGame = new BeautifulUi::OptionButton("Add a local installed game");
     addLocalInstalledGame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-    // QPushButton* addLocalInstalledGame = new QPushButton("Add a local installed game");
-    // addLocalInstalledGame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-    // connect(addLocalInstalledGame, &QPushButton::clicked, this, &AddGameWidget::addLocalGame);
+    connect(addLocalInstalledGame, &BeautifulUi::OptionButton::clicked, this, &AddGameWidget::addLocalGame);
 
     BeautifulUi::OptionButton* scanForGames = new BeautifulUi::OptionButton("Scan for games on the hard drive");
     scanForGames->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-    // QPushButton* searchGames = new QPushButton("Scan for games on the hard drive");
-    // searchGames->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
-    // connect(searchGames, &QPushButton::clicked, this, &AddGameWidget::scanForGames);
+    connect(scanForGames, &BeautifulUi::OptionButton::clicked, this, &AddGameWidget::scanForGames);
 
     QVBoxLayout* VBL = new QVBoxLayout();
     VBL->setContentsMargins(5, 5, 5, 5);
