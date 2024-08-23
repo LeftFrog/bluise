10f34a89e629f6b43513145f12fa64e834fe60e7
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-08-23 15:52:11 -0600

    Added description to options

--- src/Widgets/AddGameWidget.cpp
+++ src/Widgets/AddGameWidget.cpp
@@ -9,13 +9,13 @@
 AddGameWidget::AddGameWidget(QWidget* parent) : QWidget(parent) {
     layout = new QStackedLayout();
 
-    BeautifulUi::OptionButton* addLocalInstalledGame = new BeautifulUi::OptionButton("Add a local installed game");
+    BeautifulUi::OptionButton* addLocalInstalledGame = new BeautifulUi::OptionButton("Add a local installed game", "Add a game that is already installed on your computer");
     addLocalInstalledGame->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
     connect(addLocalInstalledGame, &BeautifulUi::OptionButton::clicked, this, &AddGameWidget::addLocalGame);
     addLocalInstalledGame->setIcon(iconHandler.getIcon(fa::fa_solid, fa::fa_folder_plus));
     addLocalInstalledGame->setFixedHeight(60);
 
-    BeautifulUi::OptionButton* scanForGames = new BeautifulUi::OptionButton("Scan for games on the hard drive");
+    BeautifulUi::OptionButton* scanForGames = new BeautifulUi::OptionButton("Scan for games on the hard drive", "Scan for games that are installed on your computer");
     scanForGames->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Minimum);
     connect(scanForGames, &BeautifulUi::OptionButton::clicked, this, &AddGameWidget::scanForGames);
     scanForGames->setIcon(iconHandler.getIcon(fa::fa_solid, fa::fa_magnifying_glass));
