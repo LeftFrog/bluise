f53112816173af4d56ce49fb4cb5404ca8aa6662
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2024-09-20 20:45:37 -0600

    Added organization name and domain

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <QtWidgets>
 #include "MainWindow.h"
+#include "Sync/GoogleDriveManager.h"
 
 int main(int argc, char** argv) {
     QApplication a(argc, argv);
+    GoogleDriveManager::getInstance();
+    a.setOrganizationName("BasedFrogs");
+    a.setApplicationName("Bluise");
+    a.setOrganizationDomain("basedfrogs.com");
     a.setWindowIcon(QIcon("/Users/leftfrog/Projects/bluise/res/1024-mac-1024.png"));
     MainWindow w;
     w.show();
