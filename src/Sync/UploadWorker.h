ce4d2a1de2ce77432c71e2ede2b46a78ef004040
Oleksandr Leonov <oleonov.leftfrog@gmail.com>
2025-01-23 20:03:14 -0700

    Implemented the UploadWorker class

--- src/Sync/UploadWorker.h
+++ src/Sync/UploadWorker.h
@@ -3,22 +3,35 @@
 //
 
 #pragma once
-#include <QObject>
+#include <QFile>
+#include <QNetworkAccessManager>
+#include <QUrl>
 
 class UploadWorker : public QObject {
     Q_OBJECT
 
 public:
-    explicit UploadWorker(const QString localFilePath, const QString folderId, QObject* parent = nullptr);
-    ~UploadWorker() override = default;
-
-public slots:
-    void process();
+    explicit UploadWorker(const QString localFilePath, const QString folderId, const QString& accessToken);
+    void startUpload();
 
 signals:
     void finished();
+    void error(const QString& error);
+
+private slots:
+    void initiateResumableSession();
+    void uploadNextChunk();
 
 private:
     QString localFilePath;
     QString folderId;
+    QString accessToken;
+    QFile* file;
+    QUrl sessionUrl;
+    QNetworkAccessManager networkManager;
+    qint64 bytesSent = 0;
+    const qint64 chunkSize = 256 * 1024;  // 256 KB
+
+    void handleSessionReply(QNetworkReply* reply);
+    void handleChunkReply(QNetworkReply* reply);
 };
